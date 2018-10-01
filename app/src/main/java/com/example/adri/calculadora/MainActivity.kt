80d2f969a3463a6925977b94dbe4f98a50dd353c
adri <adriperezNMC@outlook.com>
2018-10-01 20:14:21 +0200

    update 1

--- app/src/main/java/com/example/adri/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/adri/calculadora/MainActivity.kt
@@ -107,6 +107,8 @@ class MainActivity : AppCompatActivity() {
                 screen.setText(datos)
             }
         }
+
+        //comentario
         ce.setOnClickListener(){
             datos=""
             datos2=""
