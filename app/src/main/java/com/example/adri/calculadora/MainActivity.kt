47bf3d99a81b70a1092a8e595514323f93dffdde
adri <adriperezNMC@outlook.com>
2018-10-02 19:28:16 +0200

    update 1

--- app/src/main/java/com/example/adri/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/adri/calculadora/MainActivity.kt
@@ -1,22 +1,76 @@
 package com.example.adri.calculadora
 
+import android.annotation.SuppressLint
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-import java.math.BigDecimal
+import java.lang.Long.parseLong
 
 class MainActivity : AppCompatActivity() {
 
     var datos=""
-    var operacion="+"
+    var operacion=""
     var datos2=""
-    var datos3=""
+    var datos3: Long = 0
     var memoria=""
+    var operacionHex=""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val orientation=getResources().getConfiguration().orientation
+        if(orientation==Configuration.ORIENTATION_LANDSCAPE){
+            calHex();
+        }else{
+            calDec();
+        }
+    }
+
+    fun execHex(hex1: Long,hex2: Long){
+        when(operacionHex){
+            "+" -> datos3=(hex1+hex2)
+            "-" -> datos3=(hex1-hex2)
+            "x" -> datos3=(hex1*hex2)
+            "/" -> datos3=(hex1/hex2)
+        }
+        var hexString=java.lang.Long.toHexString(datos3)
+        resulText.text = hexString
+    }
+
+    fun hex2dec(hex: String): Long {
+        return parseLong(hex,16)
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    fun calHex(){
+        hexSum.setOnClickListener(){
+            operacionHex="+"
+            execHex(hex2dec(num1text.text.toString()), hex2dec(num2text.text.toString()))
+
+        }
+        hexRes.setOnClickListener(){
+            operacionHex="-"
+            execHex(hex2dec(num1text.text.toString()), hex2dec(num2text.text.toString()))
+
+        }
+        hexMul.setOnClickListener(){
+            operacionHex="x"
+            execHex(hex2dec(num1text.text.toString()), hex2dec(num2text.text.toString()))
+        }
+        hexDiv.setOnClickListener() {
+            operacionHex="/"
+            try {
+                execHex(hex2dec(num1text.text.toString()), hex2dec(num2text.text.toString()))
+            }catch(e: ArithmeticException){
+                resulText.text="No se puede dividir por 0"
+            }
+
+        }
+    }
+
+    fun calDec(){
         b1.setOnClickListener(){
             datos+="1"
             screen.setText(datos)
@@ -107,8 +161,6 @@ class MainActivity : AppCompatActivity() {
                 screen.setText(datos)
             }
         }
-
-
         ce.setOnClickListener(){
             datos=""
             datos2=""
@@ -129,16 +181,5 @@ class MainActivity : AppCompatActivity() {
             }
             datos=screen.text.toString()
         }
-    }
-
-    fun execute(){
-        if(datos2=="") datos2="0"
-        when(operacion){
-            "+" -> screen.setText((datos2.toDouble()+datos.toDouble()).toString());
-            "-" -> screen.setText((datos2.toDouble()-datos.toDouble()).toString());
-            "x" -> screen.setText((datos2.toDouble()*datos.toDouble()).toString());
-            "/" -> screen.setText((datos2.toDouble()/datos.toDouble()).toString());
-        }
-        datos=screen.text.toString()
     }
 }
